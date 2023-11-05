d5450ce8ad232a84805b2f420ba54957b91143e4
Wes Jones <wjones127@gmail.com>
2023-11-05 00:28:36 -0400

    initial commit

---
+++ app/routes/contacts.$contactId_.destroy.tsx
@@ -0,0 +1,13 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import invariant from "tiny-invariant";
+
+import { deleteContact } from "../data";
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData);
+  await deleteContact(params.contactId);
+  return redirect(`/`);
+};
